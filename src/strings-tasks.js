22be49d337c9fa1ad232b2b4afa9a414dd285d35
Zlatanova Irina <t0ffy@ya.ru>
2024-12-08 01:22:33 +0200

    feat: implement sumOfCodes

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -193,8 +193,15 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (!!str === false) {
+    return 0;
+  }
+  let result = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    result += str.charCodeAt(i);
+  }
+  return result;
 }
 
 /**
